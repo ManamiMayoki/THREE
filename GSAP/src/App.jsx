001e3d4ea3fd6e5e2140b252cb0a663d3cce1e78
ManamiMayoki <manamimarufa@gmail.com>
2026-09-01 11:17:16 +0600

    4 section and install gsap & @gsap/react

--- GSAP/src/App.jsx
+++ GSAP/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <main>
+      <section id="section-1">section-1</section>
+      <section id="section-2">section-2</section>
+      <section id="section-3">section-3</section>
+      <section id="section-4">section-4</section>
+    </main>
   )
 }
 
