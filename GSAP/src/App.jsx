0ce3ce32fcbc3ba31aee7c58769d717a4a5c3ad9
ManamiMayoki <manamimarufa@gmail.com>
2026-09-02 23:39:27 +0600

    trigger,markers,scrub

--- GSAP/src/App.jsx
+++ GSAP/src/App.jsx
@@ -1,26 +1,25 @@
 import './App.css'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
-import {scrollTrigger} from 'gsap/ScrollTrigger'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-
+// Register plugins once outside the component render loop
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  gsap.registerPlugin(scrollTrigger);
-
   useGSAP(() => {
     gsap.to("html", {
       backgroundColor: "maroon",
-      ScrollTrigger: {
+      scrollTrigger: {
         trigger: "#section-1",
         start: "top top",
         markers: true,
         scrub: true,
-        endTrigger: "section-4"
+        endTrigger: "#section-4",
+        end: "top top"
       }
-      // duration: 10
     });
-  }); 
+  }, []); 
 
   return (
     <main>
